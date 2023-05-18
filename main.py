186952b83cc5785aa944c3907042459f8a6e682c
dougyb83 <dougy.b@proton.me>
2023-05-18 13:45:11 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+def main():
+    pass
+
+def get_folder_to_search():
+    pass
+
+def get_destination_folder():
+    pass
+
+def get_search_from_datetime():
+    #option to enter custom date/time
+    #option to select date time from of last backup
+    pass
+
+def search_files():
+    pass
+
+def copy_files():
+    pass
+
+def save_backup_datetime():
+    pass
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
